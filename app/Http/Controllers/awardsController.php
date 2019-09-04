38555de8eff2b622cb17f087294f0162b42c7c9a
anjaligeril <anjali99malu@gmail.com>
2019-09-04 09:19:26 -0400

    update

--- app/Http/Controllers/awardsController.php
+++ app/Http/Controllers/awardsController.php
@@ -9,13 +9,15 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 class awardsController extends Controller
 {
+
+    /*show all the users*/
     public function addAwards($id){
         $user=User::find($id);
         $username=$user->name;
 
         return view('addAwards')->with(['username'=>$username,'id'=>$id]);
     }
-
+/*add the new award to database*/
 public function addNewAwards($id){
     $user=$id;
     $award=$_POST['award'];
@@ -34,12 +36,14 @@ public function addNewAwards($id){
         return back()->with ('error','Invalid input.Please fill all fields...');
     }
 }
+
+/*delete the award*/
 public function deleteAward($id){
         Award::destroy($id);
     $awards=Award::all();
     return  back()->with (['error'=>'Award deleted successfully','allAwards'=>$awards]);
 }
-
+/*update award details to database*/
     public function updateAward($id){
         $award=$_POST['award'];
         $description=$_POST['description'];
@@ -57,18 +61,20 @@ public function deleteAward($id){
             return redirect('dashboard')->with ('error','Invalid input.Please fill all fields...');
         }
     }
-
+/*search the awards*/
     public function searchAwards(){
         $id=Auth::user()->id;
         $detail=$_POST['detail'];
         $selectedAwards=Award::where('user_id','$id')->where('award', 'like','%'.$detail.'%')->orwhere('description', 'like','%'.$detail.'%')->orwhere('description', 'like','%'.$detail.'%')->get();
         return back()->with ('allAwards',$selectedAwards);
     }
+
+    /*show all awards*/
     public function showAllAwards(){
         $allawards=Award::all();
         return redirect('welcome')->with('allAwards',$allawards);
     }
-
+/*update page display*/
     public function updateAwardsInfo($id){
         $singleAward=Award::find($id);
         return view('updateAwardsInfo')->with('singleAward',$singleAward);
